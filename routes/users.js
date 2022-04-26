a895f4753aa1ea6c097d59d73b3a6afa4d910865
DjeradiMoussaab <mossab@Djeradis-iMac.local>
2022-04-26 02:02:30 +0100

    implementing user routes get/post

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../Models/user');
+const UsersService = require('../Services/users')
+const usersService = new UsersService();
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+    usersService.getAllUsers()
+        .then(users => {
+          res.json(users);
+        }).catch( err => {
+          res.status(500).send({
+            message : err.message || 'error'
+          })
+    })
+});
+
+// Return a user by ID (GET)
+router.get('/:id', async (req, res) => {
+    usersService.getUserById(req.params.id)
+        .then(user => {
+          res.json(user);
+        }).catch( err => {
+          res.status(500).send({
+          message : err.message || 'error'
+        })
+    })
+});
+
+
+// Saving a new user (POST)
+router.post('/', async (req, res) => {
+    usersService.getUserByUsername(req.body.username)
+        .then(user => {
+            console.log(user);
+            if (user != null ) {
+             res.status(409).json({
+                 message: 'user already exists!'
+             })
+            }
+            else {
+                usersService.createUser(req.body)
+                    .then(user => {
+                        res.json(user);
+                    }).catch( err => {
+                    res.status(500).send({
+                        message : err.message || 'error'
+                    })
+                })
+            }
+        }).catch( err => {
+        res.status(500).send({
+            message : err.message || 'error'
+        })
+    })
 });
 
 module.exports = router;
